3b357752108b880fa0a0b031a102bb646360d930
James Ives <iam@jamesiv.es>
2021-12-18 15:36:42 +0000

    Deploy Production Code for Commit 0fb4c54e9aafa358ab9d846e3d2e9f05420ffc7c 🚀

--- lib/util.d.ts
+++ lib/util.d.ts
@@ -2,3 +2,4 @@ import { ActionInterface } from './constants';
 export declare const isNullOrUndefined: (value: string | undefined | null) => boolean;
 export declare const checkParameters: (action: ActionInterface) => void;
 export declare const suppressSensitiveInformation: (str: string, action: ActionInterface) => string;
+export declare const extractErrorMessage: (error: unknown) => string;
